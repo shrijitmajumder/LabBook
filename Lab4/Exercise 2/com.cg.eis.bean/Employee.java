b0466bd3332461aa08e473d2190e83404f7792c9
shrijitmajumder <84119964+shrijitmajumder@users.noreply.github.com>
2021-05-19 03:58:15 +0530

    Create Employee.java

---
+++ Lab4/Exercise 2/com.cg.eis.bean/Employee.java
@@ -0,0 +1,11 @@
+package com.cg.eis.bean;
+
+public class Employee {
+	
+	public int id;
+	public String name;
+	public double salary;
+	public String designation;
+	public String insuranceScheme;
+	
+}
